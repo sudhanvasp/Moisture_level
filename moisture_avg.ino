f5fdd2524b3d038eb51577a83b4c1d87ec7cbe80
sudhanvasp <sudhanvasp@gmail.com>
2021-05-21 02:40:36 +0530

    Add files via upload

--- moisture_avg.ino
+++ moisture_avg.ino
@@ -17,8 +17,8 @@ BlynkTimer timer;
 
 void readdata()
 {
-  moisture0 = analogRead(A0);
-  moisture1 = analogRead(A1);
+  moisture0 = analogRead(enter your pin);   // edit it here
+  moisture1 = analogRead(enter your pin1);
   Blynk.virtualWrite(V0, moisture0);
   Blynk.virtualWrite(V1, moisture1);
   moistureavg = ( moisture0 + moisture1 )/2
@@ -28,7 +28,7 @@ void setup()
 {
 
   Serial.begin(9600);
-  Blynk.begin(auth,ssid,pass, "vxjblynk.duckdns.com", 8080);
+  Blynk.begin(auth,ssid,pass, "vxjblynk.duckdns.org", 8080);   //change the server if you want
   timer.setInterval(1000L, readdata); //push data every 1 sec
 }
 
